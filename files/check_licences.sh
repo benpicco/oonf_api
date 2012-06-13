89dceba769b50d710aca6ec78edacade1973f78a
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2012-06-13 16:18:30 +0200

    Remove special android includes until we know they are necessary on the latest NDKs

--- files/check_licences.sh
+++ files/check_licences.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# ../src-api/regex/* is OpenBSD
-EXCEPT="$EXCEPT -not -path '../src-api/regex/*'"
-
-# ../src-api/android/linux/* is Linux Kernel
-EXCEPT="$EXCEPT -not -path '../src-api/android/linux/*'"
-
 LEN=`cat default_licence.txt |wc -c`
 
 for file in $(eval find ../src*  -type f -name *[.][ch] $EXCEPT)
