5bfe86989cce23e56a78472b34f4c516333f69f5
Ferry Huberts <ferry.huberts@pelagic.nl>
2012-08-23 20:52:14 +0200

    fix 'git describe' invocation
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- cmake/generate_builddata.cmake
+++ cmake/generate_builddata.cmake
@@ -8,7 +8,7 @@ SET(OONF_LIB_CHANGE "")
 
 IF(NOT ${found_git} STREQUAL "found_git-NOTFOUND" AND EXISTS ${GIT})
     # everything is fine, read commit and diff stat
-    execute_process(COMMAND git describe --long --tags 
+    execute_process(COMMAND git describe --always --long --tags
         OUTPUT_VARIABLE OONF_LIB_GIT OUTPUT_STRIP_TRAILING_WHITESPACE)
     execute_process(COMMAND git diff --shortstat HEAD ./src-api/ ./src-plugins/ ./cmake ./external ./CMakeLists.txt
         OUTPUT_VARIABLE OONF_LIB_CHANGE OUTPUT_STRIP_TRAILING_WHITESPACE)
