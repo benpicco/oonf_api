f59297a334eca525a702e7223a6f06b2c36f319d
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2013-05-22 10:02:40 +0200

    Generate version number from git tag

--- cmake/generate_builddata.cmake
+++ cmake/generate_builddata.cmake
@@ -5,11 +5,18 @@ find_program(found_git git)
 
 SET(OONF_LIB_GIT "cannot read git repository")
 
-IF(NOT ${found_git} STREQUAL "found_git-NOTFOUND" AND EXISTS ${GIT})
-    # everything is fine, read commit and diff stat
-    execute_process(COMMAND git describe --always --long --tags --dirty
-        OUTPUT_VARIABLE OONF_LIB_GIT OUTPUT_STRIP_TRAILING_WHITESPACE)
+IF(NOT ${found_git} STREQUAL "found_git-NOTFOUND")
+	# get git description WITH dirty flag
+	execute_process(COMMAND git describe --always --long --tags --dirty --match "v[0-9]*"
+		OUTPUT_VARIABLE OONF_LIB_GIT OUTPUT_STRIP_TRAILING_WHITESPACE)
 ENDIF()
+
+# compare with version number
+STRING(REGEX MATCH "v${OONF_VERSION}-" FOUND_VERSION ${OONF_LIB_GIT})
+IF (NOT FOUND_VERSION)
+	message (FATAL_ERROR "Library version '${OONF_VERSION}' is not present in git description '${OONF_LIB_GIT}'."
+		"Please re-run 'cmake ..' to update build files")
+ENDIF (NOT FOUND_VERSION)
 
 # create builddata file
 configure_file (${SRC} ${DST})
